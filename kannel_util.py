a517d46877bc5176bb67d5a8501bca1399bcfadd
Rene Feiner <rene.feiner@mobiletulip.com>
2012-02-10 11:54:32 +0100

    * Make compatible with Kannel bearer box 1.4.3

--- kannel_util.py
+++ kannel_util.py
@@ -13,16 +13,17 @@ def get_status(url = os.environ['STATUS_URL']):
 
     el = xml.find('sms')
     result_d = result['sms'] = {}
-    result_d['received'] = {'total': int(el.findtext('received/total')),
-     'queued': int(el.findtext('received/queued'))}
+    result_d['received'] = {'total': 0,
+     'queued': 0}
     result_d['sent'] = {'total': int(el.findtext('sent/total')),
      'queued': int(el.findtext('sent/queued'))}
     result_d['storesize'] = int(el.findtext('storesize'))
 
-    el = xml.find('dlr')
     result_d = result['dlr'] = {}
     result_d['received'] = {'total': int(el.findtext('received/total'))}
-    result_d['sent'] = {'total': int(el.findtext('sent/total'))}
+    result_d['sent'] = {'total': 0}
+
+    el = xml.find('dlr')
     result_d['queued'] = int(el.findtext('queued'))
 
     els = xml.find('smscs').findall('smsc')
@@ -31,16 +32,10 @@ def get_status(url = os.environ['STATUS_URL']):
         result_d.append({
             'id': el.findtext('id'),
             'admin_id': el.findtext('admin-id'),
-            'received': {
-                'sms': int(el.findtext('received/sms')),
-                'dlr': int(el.findtext('received/dlr'))
-            },
-            'sent': {
-                'sms': int(el.findtext('sent/sms')),
-                'dlr': int(el.findtext('sent/dlr'))
-            },
+            'received':int(el.findtext('received')),
+            'sent': int(el.findtext('sent')),
             'failed' : int(el.findtext('failed')),
-            'queued' : int(el.findtext('failed')),
+            'queued' : int(el.findtext('queued')),
             'status' : el.findtext('status').split(' ', 2)[0]
         })
 
