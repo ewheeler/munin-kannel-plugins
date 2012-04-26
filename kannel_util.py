7428b85f8790247b5b9e89d98964b561d61f7a36
Rene Feiner <rene.feiner@mobiletulip.com>
2012-04-26 15:58:18 +0200

    Make compatible with kannel 1.5 (again) & extra plugins
    * Added throughput plugin for all outgoing traffic (sent SMS & received
    DLRs)
    * Added throughput plugin for traffic per SMSC (sent SMS & received
    DLRs)

--- kannel_util.py
+++ kannel_util.py
@@ -13,17 +13,17 @@ def get_status(url = os.environ['STATUS_URL']):
 
     el = xml.find('sms')
     result_d = result['sms'] = {}
-    result_d['received'] = {'total': 0,
-     'queued': 0}
+    result_d['received'] = {'total': int(el.findtext('received/total')),
+     'queued': int(el.findtext('received/queued'))}
     result_d['sent'] = {'total': int(el.findtext('sent/total')),
      'queued': int(el.findtext('sent/queued'))}
     result_d['storesize'] = int(el.findtext('storesize'))
 
+    el = xml.find('dlr')
     result_d = result['dlr'] = {}
     result_d['received'] = {'total': int(el.findtext('received/total'))}
-    result_d['sent'] = {'total': 0}
+    result_d['sent'] = {'total': int(el.findtext('sent/total'))}
 
-    el = xml.find('dlr')
     result_d['queued'] = int(el.findtext('queued'))
 
     els = xml.find('smscs').findall('smsc')
@@ -32,8 +32,14 @@ def get_status(url = os.environ['STATUS_URL']):
         result_d.append({
             'id': el.findtext('id'),
             'admin_id': el.findtext('admin-id'),
-            'received':int(el.findtext('received')),
-            'sent': int(el.findtext('sent')),
+            'received': {
+                'sms': int(el.findtext('sms/received')),
+                'dlr': int(el.findtext('dlr/received'))
+            },
+            'sent': {
+                'sms': int(el.findtext('sms/sent')),
+                'dlr': int(el.findtext('dlr/sent'))
+            },
             'failed' : int(el.findtext('failed')),
             'queued' : int(el.findtext('queued')),
             'status' : el.findtext('status').split(' ', 2)[0]
