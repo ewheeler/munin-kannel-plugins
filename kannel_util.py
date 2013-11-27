85ee5595ed19b3a3b68b84ec9e13280e1a4fd434
ewheeler <evanmwheeler@gmail.com>
2013-11-27 10:34:27 +0300

    provide zeros for missing dlr values if NO_DLR

--- kannel_util.py
+++ kannel_util.py
@@ -3,7 +3,7 @@ from xml.etree.ElementTree import XML
 import os
 
 
-def get_status(url = os.environ['STATUS_URL'], no_dlr = os.environ['NO_DLR']):
+def get_status(url=os.environ['STATUS_URL'], no_dlr=os.environ['NO_DLR']):
     response = urllib2.urlopen(url)
     try:
         xml = XML(response.read())
@@ -15,9 +15,9 @@ def get_status(url = os.environ['STATUS_URL'], no_dlr = os.environ['NO_DLR']):
     el = xml.find('sms')
     result_d = result['sms'] = {}
     result_d['received'] = {'total': int(el.findtext('received/total')),
-     'queued': int(el.findtext('received/queued'))}
+                            'queued': int(el.findtext('received/queued'))}
     result_d['sent'] = {'total': int(el.findtext('sent/total')),
-     'queued': int(el.findtext('sent/queued'))}
+                        'queued': int(el.findtext('sent/queued'))}
     result_d['storesize'] = int(el.findtext('storesize'))
 
     el = xml.find('dlr')
@@ -25,6 +25,9 @@ def get_status(url = os.environ['STATUS_URL'], no_dlr = os.environ['NO_DLR']):
     if not no_dlr:
         result_d['received'] = {'total': int(el.findtext('received/total'))}
         result_d['sent'] = {'total': int(el.findtext('sent/total'))}
+    else:
+        result_d['received'] = {'total': 0}
+        result_d['sent'] = {'total': 0}
 
     result_d['queued'] = int(el.findtext('queued'))
 
@@ -43,9 +46,9 @@ def get_status(url = os.environ['STATUS_URL'], no_dlr = os.environ['NO_DLR']):
                     'sms': int(el.findtext('sms/sent')),
                     'dlr': int(el.findtext('dlr/sent'))
                 },
-                'failed' : int(el.findtext('failed')),
-                'queued' : int(el.findtext('queued')),
-                'status' : el.findtext('status').split(' ', 2)[0]
+                'failed': int(el.findtext('failed')),
+                'queued': int(el.findtext('queued')),
+                'status': el.findtext('status').split(' ', 2)[0]
             })
         else:
             result_d.append({
@@ -53,13 +56,15 @@ def get_status(url = os.environ['STATUS_URL'], no_dlr = os.environ['NO_DLR']):
                 'admin_id': el.findtext('admin-id'),
                 'received': {
                     'sms': int(el.findtext('received')),
+                    'dlr': 0
                 },
                 'sent': {
                     'sms': int(el.findtext('sent')),
+                    'dlr': 0
                 },
-                'failed' : int(el.findtext('failed')),
-                'queued' : int(el.findtext('queued')),
-                'status' : el.findtext('status').split(' ', 2)[0]
+                'failed': int(el.findtext('failed')),
+                'queued': int(el.findtext('queued')),
+                'status': el.findtext('status').split(' ', 2)[0]
             })
 
     return result
